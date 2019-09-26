5f5164d20ef4ee314d523f1d33a229922b110dba
Colin Jaffe <balloonasaurus@gmail.com>
2019-09-26 13:35:48 -0400

    Initial commit.

---
+++ main.js
@@ -0,0 +1,35 @@
+function capitalize() {
+}
+
+function exclaim() {
+}
+
+function firstCharacter() {
+}
+
+function lastCharacter() {
+}
+
+function oneCharacter() {
+}
+
+function twoCharacters() {
+}
+
+function initials() {
+}
+
+function yeller() {
+}
+
+
+module.exports = {
+  capitalize,
+  exclaim,
+  firstCharacter,
+  lastCharacter,
+  oneCharacter,
+  twoCharacters,
+  initials,
+  yeller,
+};
---
+++ main.test.js
@@ -0,0 +1,83 @@
+const {
+  capitalize,
+  exclaim,
+  firstCharacter,
+  lastCharacter,
+  oneCharacter,
+  twoCharacters,
+  initials,
+  yeller,
+} = require('./main');
+
+
+describe('capitalize', () => {
+  test('returns upper cased version of string', () => {
+    expect(capitalize('Speed Racer Was Great'))
+      .toBe('SPEED RACER WAS GREAT');  });
+});
+
+describe('exclaim', () => {
+  test('adds an exclamation point at the end of the given string', () => {
+    expect(exclaim('hello')).toBe('hello!')
+  })
+
+  test('adds a second exclamation point if there already is one', () => {
+    expect(exclaim('hello!')).toBe('hello!!')
+  })
+})
+
+describe('firstCharacter', () => {
+  test('returns only the first character of a string', () => {
+    expect(firstCharacter('Colin')).toBe('C');
+  })
+
+  test('returns a space if the string begins with one', () => {
+    expect(firstCharacter(' Spacey McSpacerson')).toBe(' ');
+  })
+})
+
+describe('lastCharacter', () => {
+  test('returns only the last character of a string', () => {
+    expect(lastCharacter('mcLasterton')).toBe('n');
+  })
+
+  test('returns a space if the string ends with one', () => {
+    expect(lastCharacter('Spacey McSpacerson ')).toBe(' ')
+  })
+})
+
+describe('oneCharacter', () => {
+  test('returns an arbitrary character', () => {
+    expect(oneCharacter('hello there', 8)).toBe('e');
+  })
+})
+
+describe('twoCharacters', () => {
+  test('returns arbitrary characters from the given string', () => {
+    expect(twoCharacters('I want a bagel!', 2, 11)).toBe('wg')
+  })
+
+  test('returns the same character twice if given the same index twice', () => {
+
+  })
+})
+
+describe('initials', () => {
+  test('returns the initials of a full name', () => {
+    expect(initials('Herman Melville')).toBe('H.M.')
+  });
+
+  test('returns the first initial twice if given only one name', () => {
+    expect(initials('Colin')).toBe('C.C.');
+  })
+
+  test('returns only two initials if given three names', () => {
+    expect(initials('Colin Raymond Jaffe')).toBe('C.R.')
+  })
+})
+
+describe('yeller', () => {
+  test('adds three initials and capitalizes', () => {
+    expect(yeller('hey')).toBe('HEY!!!');
+  })
+})
